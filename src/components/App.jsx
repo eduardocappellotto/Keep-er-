24252c0ae5c98438008807f2f00a1c8f1cb1904b
eduardocappellotto <51893328+eduardocappellotto@users.noreply.github.com>
2020-02-26 15:18:06 -0300

    V0.2
    Adicionados arrow functions, uma lista base de notas (notes.js) e utilzados componentes e função map.

---
+++ src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function App() {
+  return (
+    <div>
+      <Header />
+
+      {notes.map(noteContent => (
+        <Note
+          key={noteContent.id}
+          title={noteContent.title}
+          content={noteContent.content}
+        />
+      ))}
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
