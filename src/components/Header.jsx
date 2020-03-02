bedb7c6fc917d0eaa9db4e03c0f355d62a30b00a
eduardocappellotto <51893328+eduardocappellotto@users.noreply.github.com>
2020-03-02 11:53:47 -0300

    Update Header.jsx
    Adicionado material ui import para o ícone do header da aplicação.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import WbIncandescentIcon from '@material-ui/icons/WbIncandescent';
 
 function Header(){
 
     return(
-    <header><h1>Keeper App</h1></header>
+    <header><h1><WbIncandescentIcon/> Keep-er App</h1></header>
     )
     
 }
